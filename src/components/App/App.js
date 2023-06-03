31fd99aa2be493118d971f9d7bf15aad74bc4979
Mayis Vardanyan <crftlf@gmail.com>
2023-06-03 18:11:09 +0400

    Try deploy project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -57,7 +57,7 @@ class App extends Component {
                             <ErrorBoundary>
                                 <HeroSidebarInfo heroId={selectedHero} />
                             </ErrorBoundary>
-                            <FormApp />
+                            {/* <FormApp /> */}
                         </section>
                     </div>
                 </main>
